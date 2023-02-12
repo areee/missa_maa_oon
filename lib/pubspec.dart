27f5ab2894dd0cbb65eaa080bb8bc283ecc32b25
areee <10089872+areee@users.noreply.github.com>
2023-02-12 22:09:17 +0200

    Add initially pubspec.dart

---
+++ lib/pubspec.dart
@@ -0,0 +1,62 @@
+// This file is generated automatically, do not modify
+
+// ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
+
+mixin Pubspec {
+  static final buildDate = DateTime.utc(2023, 2, 12, 18, 40, 46);
+
+  static const name = 'missa_maa_oon';
+
+  static const description = 'Save your current location';
+
+  static const publish_to = 'none';
+
+  static const versionFull = '0.2.0+1';
+
+  static const version = '0.2.0';
+
+  static const versionSmall = '0.2';
+
+  static const versionMajor = 0;
+
+  static const versionMinor = 2;
+
+  static const versionPatch = 0;
+
+  static const versionBuild = 1;
+
+  static const versionPreRelease = '';
+
+  static const versionIsPreRelease = false;
+
+  static const environment = <dynamic, dynamic>{
+    'sdk': '>=2.19.1 <3.0.0',
+  };
+
+  static const dependencies = <dynamic, dynamic>{
+    'flutter': <dynamic, dynamic>{
+      'sdk': 'flutter',
+    },
+    'cupertino_icons': '^1.0.2',
+    'geolocator': '^9.0.2',
+    'share_plus': '^6.3.0',
+    'isar': '^3.0.5',
+    'isar_flutter_libs': '^3.0.5',
+    'intl': '^0.18.0',
+    'about': '^2.1.1',
+  };
+
+  static const dev_dependencies = <dynamic, dynamic>{
+    'flutter_test': <dynamic, dynamic>{
+      'sdk': 'flutter',
+    },
+    'flutter_lints': '^2.0.0',
+    'isar_generator': '^3.0.5',
+    'build_runner': '^2.3.3',
+    'pubspec_extract': '^2.0.5',
+  };
+
+  static const flutter = <dynamic, dynamic>{
+    'uses-material-design': true,
+  };
+}
